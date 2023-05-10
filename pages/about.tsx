01bd5702cbe5b7a89417af9cfc9447075d538898
Serzh108 <serzh@gabris.kiev.ua>
2023-05-10 17:30:22 +0300

    EDIT | before pull origin

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,16 @@
+'use client';
+import { Title } from './styles';
+import styled from 'styled-components';
+
+export const Title1 = styled.h1`
+  font-family: var(--font-roboto), sans-serif;
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
 const About = () => {
-  return <div>About page</div>
+  return <Title>about NEW</Title>;
 };
 
 export default About;
---
+++ pages/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+font-family: var(--font-roboto), sans-serif;
+  font-size: 1.5em;
+  text-align: center;
+  color: purple;
+`;
