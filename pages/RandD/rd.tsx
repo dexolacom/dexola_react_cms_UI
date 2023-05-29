616ff0b75304629af2feb05e2f885bf492974e58
Serzh108 <serzh@gabris.kiev.ua>
2023-05-29 11:11:54 +0300

    FIX | fixed according comments on github

--- pages/RandD/rd.tsx
+++ pages/RandD/rd.tsx
@@ -2,28 +2,13 @@
 
 import PageContainer from '../../components/PageContainer/PageWrapper';
 import { RandD_INFO } from '../../constants/textConstants';
+import TypeItem from '../partnership/typeItem';
+import { useTypeStatus } from '../../utils/utils';
 import styles from './styles.module.css';
 import styles2 from '../../components/PageContainer/pageContainer.module.css';
-import { useState } from 'react';
-import TypeItem from '../partnership/typeItem';
-
-// --- it's like as in Partnership !!! ---
-const INIT_TYPE_STATUS = Array(5).fill(false);
 
 const RandD = () => {
-  // --- same as in Partnership !!! ---
-  const [typeStatus, setTypeStatus] = useState<boolean[]>(INIT_TYPE_STATUS);
-
-  const clickHandler = (e: React.SyntheticEvent) => {
-    const id = +e?.currentTarget?.id;
-    if (typeStatus.length > 0) {
-      const newStatusArray = typeStatus.map((el, idx) =>
-        idx === id && !el ? true : false,
-      );
-      setTypeStatus(newStatusArray);
-    }
-  };
-  // --- / same as in Partnership !!! ---
+  const { typeStatus, clickHandler } = useTypeStatus(5);
   return (
     <PageContainer title={'R&D'} link={'some link'}>
       <p className={styles2.contentText}>
